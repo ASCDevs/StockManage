64af90f6bcd2684f03e733ed1529cdb76c414db7
ALEXANDRE SANTOS CAVALCANTE <alexandre.santos15@hcfmusp-pa.phcnet.usp.br>
2021-02-17 12:59:15 -0200

    New features

--- Aplicacao/EnderecoAplicacao.cs
+++ Aplicacao/EnderecoAplicacao.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dominio;
+using Dominio.contrato;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,31 @@ namespace Aplicacao
 {
     public class EnderecoAplicacao
     {
-        //private readonly IRepositorio<Endereco> repositorio;
+        private readonly IRepositorio<Endereco> repositorio;
+
+        public EnderecoAplicacao(IRepositorio<Endereco> repo)
+        {
+            repositorio = repo;
+        }
+
+        public void Salvar(Endereco endereco)
+        {
+            repositorio.Salvar(endereco);
+        }
+
+        public void Excluir(Endereco endereco)
+        {
+            repositorio.Excluir(endereco);
+        }
+
+        public IEnumerable<Endereco> ListarTodos()
+        {
+            return repositorio.ListarTodos();
+        }
+
+        public Endereco ListarPorId(string id)
+        {
+            return repositorio.ListarPorId(id);
+        }
     }
 }
---
+++ Aplicacao/EnderecoAplicacaoConstrutor.cs
@@ -0,0 +1,17 @@
+﻿using RepositorioADO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacao
+{
+    public class EnderecoAplicacaoConstrutor
+    {
+        public static ProdutoAplicacao ProdutoAplicacaoADO()
+        {
+            return new ProdutoAplicacao(new ProdutoRepositorioADO());
+        }
+    }
+}

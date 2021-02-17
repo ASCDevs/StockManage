7d6198cb49ff0ff3215b90bc8bd6ae351ca3a4d0
ALEXANDRE SANTOS CAVALCANTE <alexandre.santos15@hcfmusp-pa.phcnet.usp.br>
2021-02-17 13:01:40 -0200

    General Updates

--- Dominio/Cliente.cs
+++ Dominio/Cliente.cs
@@ -7,9 +7,9 @@ namespace Dominio
     public class Cliente
     {
         //Adicionar tratamento para ver se já existe aquele CPF Cadastrado
-        [Required(ErrorMessage = "Obrigatório o preenchimento do CPF")]
         [DisplayName("CPF")]
-        [Remote(action: "verificaCpfUnico", controller: "~/WebUI/Controllers/ClienteController", ErrorMessage = "CPF já existente")]
+        [Remote("verificaCpfUnico", "Cliente", ErrorMessage = "CPF já existente")]
+        [Required(ErrorMessage = "Obrigatório o preenchimento do CPF")]
         public string cpf { get; set; }
 
         [Required(ErrorMessage = "Preencha o nome do cliente")]

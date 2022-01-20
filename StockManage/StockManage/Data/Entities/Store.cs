24401e00dc6946a2f1db253b3810a5a3c1d1a265
Alexandre Santos <ale.santos49@gmail.com>
2022-01-19 22:40:45 -0300

    Preparado o context

---
+++ StockManage/StockManage/Data/Entities/Store.cs
@@ -0,0 +1,12 @@
+﻿namespace StockManage.Data.Entities
+{
+    public class Store
+    {
+        public int id_store { get; set; }
+        public string store_name { get; set; }
+        public string store_address { get; set; }
+        public string store_city { get; set; }
+        public string store_district { get; set; }
+        public string store_country { get; set; }
+    }
+}
--- StockManage/StockManage/Data/StorageContext.cs
+++ StockManage/StockManage/Data/StorageContext.cs
@@ -1,9 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using StockManage.Data.Entities;
 
 namespace StockManage.Data
 {
     public class StorageContext : DbContext
     {
+
+        public StorageContext(DbContextOptions<StorageContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Storage> Storage { get; set; }
+        public DbSet<OrderStatus> OrderStatus { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
 
     }
 }
